3385f3bc68a297cfb59d4e41e5faead3cba56536
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:51:33 -0600

    Update Build.java

--- workshops/WS(9-20-18)/src/ejercicio/Build.java
+++ workshops/WS(9-20-18)/src/ejercicio/Build.java
@@ -1,24 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
 
-/**
- *
- * @author PJ129
- */
-public class Build {
 
+public class Build {
+    //variables con clases creadas por nosotros
     private final Impresion impresion;
     private final Fotocopia fotocopia; 
     private final Levantado levantado;
     private final CorreccionOrtografica correccion;
     private final DisenoGrafico diseno;
     
-
+// cosntructor con parámetros
     public Build(Impresion impresion, Fotocopia fotocopia  , Levantado levantado, CorreccionOrtografica correccion, DisenoGrafico diseno) {
         this.impresion = impresion;
         this.fotocopia = fotocopia;
@@ -26,22 +18,22 @@ public class Build {
         this.correccion = correccion;
         this.diseno = diseno;
     }
-    
+    //método set con parámetros
     public void setInfo (int cantImpresion, int cantFotocopia, int cantLevantado, int cantCorreccion, String tipoCorreccion, int cantHours, String tipoDiseno   ){
-        this.impresion.setCant(cantImpresion);
-        this.fotocopia.setCost(cantFotocopia);
-        this.levantado.setCant(cantLevantado);
-        this.correccion.setInfo(cantCorreccion,tipoCorreccion);
-        this.diseno.setInfo(cantHours, tipoDiseno);
+        this.impresion.setCant(cantImpresion);// le pasamos la cantidad a impresion
+        this.fotocopia.setCost(cantFotocopia);// le pasamos la cantidad a fotocopia
+        this.levantado.setCant(cantLevantado);//cantidad a levantado
+        this.correccion.setInfo(cantCorreccion,tipoCorreccion);//le pasamos la cantidad y el tipo
+        this.diseno.setInfo(cantHours, tipoDiseno);// las horas y el tipo de diseño
         
     }
 
-    
+    // imprimir
     public String getPrintTotal() {
     
         return "Total a pagar: "+ (this.impresion.getCost()+ this.fotocopia.getCost() + this.levantado.getCost() + this.correccion.getCost()+this.diseno.getCost());
         
-    }
+    }//imprime
     public String getPrintCost(){
         return "Costo por impresion: "+this.impresion.getCost()+"\nCostopor fotocopias: "+this.fotocopia.getCost()+"\nCosto por Levantado: "+this.levantado.getCost()
                 +"\nCosto por Correccion: "+this.correccion.getCost()+"\nCosto por Diseno: "+this.diseno.getCost();
