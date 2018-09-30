8c0a618bb14d08596ddcb43c446650b39a7311cc
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:52:31 -0600

    Update CorreccionOrtografica.java

--- workshops/WS(9-20-18)/src/ejercicio/CorreccionOrtografica.java
+++ workshops/WS(9-20-18)/src/ejercicio/CorreccionOrtografica.java
@@ -1,38 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
-public enum CorreccionOrtografica {
-    CO(800, 0, "");
+
+public enum CorreccionOrtografica {//clase de tipo enum
+    CO(800, 0, ""); // con 3 paraémtros
+    //variables 
     private final double cost;
     private double cant;
     private String type;
-
+    // cosntructor con paármetros
     private CorreccionOrtografica(double cost, double cant, String type) {
         this.cost = cost;
         this.cant = cant;
         this.type = type;
     }
-
+//método set con parámetros
     public void setInfo(double cant, String type) {
         this.cant = cant;
         this.type = type;
     }
-
+//método get 
     public double getCost() {
-        if (type.equals("Estudiante")) {
-            return this.cost * cant - (cant * 150);
-        } else if (type.equals("Revista")) {
-            return this.cost * cant + (cant * 100);
+        if (type.equals("Estudiante")) {// si es igual a estudiante
+            return this.cost * cant - (cant * 150);//costo por cantidad menos cantidad por 150
+        } else if (type.equals("Revista")) {//si es igual a estudiante
+            return this.cost * cant + (cant * 100);//costo por cantidad más cantidad por 150
         } else if (type.equals("N/A")) {
-            return this.cost * cant;
+            return this.cost * cant;//sino simplemente costo por cantidad
         } else {
             return 0;
         }
