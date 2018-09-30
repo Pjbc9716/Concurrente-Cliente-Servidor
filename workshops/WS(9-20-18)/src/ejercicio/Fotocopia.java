24c64f1aa3585264632d093f17ab3397a95f4f70
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:54:04 -0600

    Update Fotocopia.java

--- workshops/WS(9-20-18)/src/ejercicio/Fotocopia.java
+++ workshops/WS(9-20-18)/src/ejercicio/Fotocopia.java
@@ -1,32 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
+
+ //clase tipo: enum
 public enum Fotocopia {
     FBN(10,0), FC(15,0);
+    //variables costo y cantidad
     private final double cost;
     private int cant;
+    //cosntructor con parámetros
     private Fotocopia(double cost, int cant) {
         this.cost = cost;
         this.cant = cant;
     }
-//    
+//método set para pasarle la cantidad
     public void setCost(int cant){
         this.cant= cant;
     }
 
     public double getCost() {
         if (this == FBN) {
-            return this.cost * cant;
+            return this.cost * cant;//multiplica precio por cantidad
         } else if (this == FC) {
-            return this.cost * cant;
+            return this.cost * cant;//multiplica el precio por cantidad
         } else {
             return 0;
         }
