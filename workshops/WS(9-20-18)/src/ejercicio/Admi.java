1dadf32310420832372d05dbca42a83deff1c33c
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:50:46 -0600

    Update Admi.java

--- workshops/WS(9-20-18)/src/ejercicio/Admi.java
+++ workshops/WS(9-20-18)/src/ejercicio/Admi.java
@@ -1,27 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
+
 public class Admi {
     
     private Build build;
-    
+    //constructor vacío
     public Admi() {
     }
     
     public void newObject() {
-
+   // lo instanciamos y le pasamos los parámetros
         build = new Build(Impresion.IC, Fotocopia.FBN, Levantado.MP,CorreccionOrtografica.CO, DisenoGrafico.Diseno);
-        
+        // le pasamos datos al método setinf de la clase build 
         build.setInfo(1, 1, 65, 434, "Estudiante", 500, "Usado");
-
+        //imprimir datos con el método get print
         System.out.println(build.getPrintCost()+"\n--------------------\n"+build.getPrintTotal());
     }
 
