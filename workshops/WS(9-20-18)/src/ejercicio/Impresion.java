3710ada72d3caa48664907473b1c5b74bb2cb258
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:54:28 -0600

    Update Impresion.java

--- workshops/WS(9-20-18)/src/ejercicio/Impresion.java
+++ workshops/WS(9-20-18)/src/ejercicio/Impresion.java
@@ -1,32 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
+//clase tipo: enum
 public enum Impresion {
     IBN(10,0), IC(15,0);
+    //variables costo y cantidad
     private final double cost;
     private int cant;
-
+    //cosntructor con parámetros
     private Impresion(double cost, int cant) {
         this.cost = cost;
         this.cant = cant;
     }
+    //método set para pasarle la cantidad
     public void setCant (int cant){
         this.cant = cant;
     }
-
+   //método get costo
     public double getCost() {
         if (this == IBN) {
             return this.cost * cant;
+            //nos devuelve el precio DE BN multiplicado por la cantidad
         } else if (this == IC) {
             return this.cost * cant;
+            //nos devuelve el precio de color multiplicado por la cantidad
         } else {
             return 0;
         }
