a5cba74e5c4aba2ec544b14510fb4cc39a203e53
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:53:01 -0600

    Update DisenoGrafico.java

--- workshops/WS(9-20-18)/src/ejercicio/DisenoGrafico.java
+++ workshops/WS(9-20-18)/src/ejercicio/DisenoGrafico.java
@@ -1,36 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
+
 public enum DisenoGrafico {
-    Diseno (20000,0,"");
+    Diseno (20000,0,"");//de tipo num con diferentes parámetros
+    //variables costo, horas, tipo
     private final double cost;
     private int hour;
     private String type;
-
+    //cosntructor con parámetros
     private DisenoGrafico(double cost, int hour, String type) {
         this.cost = cost;
         this.hour = hour;
         this.type = type;
     }
-    
+    //método set con parámetros con horas y tipo
     public void setInfo (int hour, String type){
         this.hour = hour;
         this.type = type;
     }
-    
+    //método get 
     public double getCost ( ){
-        if (type.equals("Nuevo")){
-            return this.cost*hour;
-        }else if (type.equals("Usado")){
-            return (this.cost*hour-(5000*hour));
+        if (type.equals("Nuevo")){ // si el tipo es igual a nuevo
+            return this.cost*hour; //multiplica costo por horas
+        }else if (type.equals("Usado")){//si el tipo es igual a usado 
+            return (this.cost*hour-(5000*hour));//mutiplica costopor horas menos 5000 multiplicado por la cantidad de horas
         }else{
             return 0;
         }
