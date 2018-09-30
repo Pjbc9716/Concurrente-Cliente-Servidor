31ad859ac7649e0941d7854baefb1e118ed8f98f
Dean FB <43184649+deanminik@users.noreply.github.com>
2018-09-29 20:54:57 -0600

    Update Levantado.java

--- workshops/WS(9-20-18)/src/ejercicio/Levantado.java
+++ workshops/WS(9-20-18)/src/ejercicio/Levantado.java
@@ -1,38 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio;
 
-/**
- *
- * @author PJ129
- */
+  //clase tipo: enum
 public enum Levantado {
     PC(150,0), MP(100,0);
+    //variables costo y cantidad
     private final double cost;
     private int cant;
-
+//cosntructor con parámetros
     private Levantado(double cost, int cant) {
         this.cost = cost;
         this.cant = cant;
     }
+    //método set para pasarle la cantidad
     public void setCant (int cant){
         this.cant = cant;
     }
+    //método get costo
     public double getCost() {
-        if (cant <500) {
+        if (cant <500) {//si es menor a 500
             if (this == PC) {
-                return this.cost * cant;
+                return this.cost * cant; //precio de PC por cantidad
             } else if (this == MP) {
-                return this.cost * cant;
+                return this.cost * cant;//precio de MP por cantidad
             } else {
                 return 0;
             }
-        } else if (cant >= 500) {
+        } else if (cant >= 500) {// sino si cantidad es mayor o igual 
             if (this == PC) {
-                return ((this.cost * cant) - (130 * cant));
+                return ((this.cost * cant) - (130 * cant));// precio por cantidad menos la cantidad de paginas por 130
             } else if (this == MP) {
                 return ((this.cost * cant) - (130 * cant));
             } else {
