8a0b0892ab598589a3b97086701fb60fb43afa71
Manuel <manuchenlin@gmail.com>
2018-10-11 19:11:59 -0600

    Prueba

---
+++ workshops/Prueba/src/concusemana1/ConcuSemana1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concusemana1;
+
+/**
+ *
+ * @author Manuel
+ */
+public class ConcuSemana1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int n =100;
+    int[]arreglo=new int[n];
+    int random=1000;
+    max= 0;min
+    
+    for(int i=0;i<arreglo.length;i++){
+        arreglo[i]=(int)(Math.random()+random);
+        
+    }
+    for(int i=0;i<arreglo.length;i++){
+        if(max < arreglo[i]){
+            m
+        }
+    }
+    
+        // TODO code application logic here
+    }
+    
+}
